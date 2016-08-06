eb94b12c2d3143576be2e56b4d182a8fe63873a6
THACHER <thachert1d@gmail.com>
2016-08-05 18:13:16 -0700

    initial commit with materialize and angularjs

---
+++ app.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict'
+  angular
+  .module('tripods', ['ui.materialize'])
+
+  .controller('BodyController', ["$scope", function ($scope) {
+    $scope.select = {
+      value: "Option1",
+      choices: ["Option1", "I'm an option", "This is materialize", "No, this is Patrick."]
+    };
+  }]);
+})();
